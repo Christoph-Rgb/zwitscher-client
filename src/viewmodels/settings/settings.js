6dc9dc0d23c088736eda5d0329c7f74d6d46ea18
Christoph <chris.schwaegerl@gmail.com>
2017-01-08 20:58:38 +0100

    validation when changing account settings

--- src/viewmodels/settings/settings.js
+++ src/viewmodels/settings/settings.js
@@ -21,6 +21,7 @@ export class Settings {
       this.refreshUser()
     }));
     this.refreshUser();
+    initializeFormValidation(this.update.bind(this), null);
   }
 
   detached() {
@@ -30,6 +31,10 @@ export class Settings {
   }
 
   update() {
+    if (this.loggedInUser.password === '') {
+      this.loggedInUser.password = this.loggedInUser.oldPassword;
+    }
+
     this.zwitscherService.updateUser(this.loggedInUser).then(updatedUser => {
       //refreshes the token after user was updated
       this.zwitscherService.reAuhtenticate(updatedUser.email, updatedUser.password);
@@ -44,5 +49,95 @@ export class Settings {
     this.loggedInUser = this.zwitscherService.getLoggedInUser();
     this.loggedInUser.oldPassword = '';
     this.loggedInUser.password = '';
+    this.loggedInUser.passwordConfirm = '';
   }
+}
+
+function initializeFormValidation(onSuccessFunction, onFailureFunction) {
+  $('#settingsForm')
+    .form({
+      on: 'submit',
+      inline: true,
+      onSuccess: function(event){
+        //prevents redirect
+        event.preventDefault();
+        onSuccessFunction();
+      },
+      onFailure: function(){
+        onFailureFunction();
+        return false;
+      },
+      fields: {
+        firstName: {
+          identifier  : 'firstName',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Enter your first name'
+            }
+          ]
+        },
+        lastName: {
+          identifier  : 'lastName',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Enter your last name'
+            }
+          ]
+        },
+        gender: {
+          identifier  : 'gender',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Provide your gender'
+            }
+          ]
+        },
+        email: {
+          identifier  : 'email',
+          rules: [
+            {
+              type   : 'email',
+              prompt : 'Enter valid email address'
+            }
+          ]
+        },
+        oldPassword: {
+          identifier  : 'oldPassword',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Enter old password'
+            }
+          ]
+        },
+        password: {
+          identifier  : 'password',
+          optional: true,
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Enter password'
+            },
+            {
+              type   : 'minLength[6]',
+              prompt : 'Password must be 6 or more characters'
+            }
+          ]
+        },
+        passwordConfirm: {
+          identifier  : 'passwordConfirm',
+          optional: true,
+          rules: [
+            {
+              type   : 'match[password]',
+              prompt : 'Passwords do not match'
+            }
+          ]
+        }
+      }
+    })
+  ;
 }
